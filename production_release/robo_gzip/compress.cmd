cad80e919a29b2df5e93e5f72bbaf7093d871162
Jack Giffin <25795277+anonyco@users.noreply.github.com>
2018-12-02 19:41:53 -0500

    Update compress.cmd

--- production_release/robo_gzip/compress.cmd
+++ production_release/robo_gzip/compress.cmd
@@ -1,4 +1,5 @@
 @echo off
+if not "%1"=="" (echo.%1&%~dps0static-compress -c zopfli -j 8 -e .gz %11>nul&%~dps0static-compress -c brotli -j 8 -e .br -q 0 %11>nul&exit /b 0)
 if exist "__last_run__" (set /p lastdate=<__last_run__)
 if "%lastdate%"=="" (
 set "lastdate="
@@ -8,11 +9,12 @@ set "lastdate=/d +%lastdate%"
 echo. Compressing all changed files...
 echo.
 cd ..
-forfiles.exe /s %lastdate% /m "*.css" /c "cmd /c if @isdir==FALSE (echo.@file&%~dps0static-compress -c zopfli -j 8 -e .gz @file1>nul&%~dps0static-compress -c brotli -j 8 -e .br -q 0 @file1>nul)"
-forfiles.exe /s %lastdate% /m "*.js" /c "cmd /c if @isdir==FALSE (echo.@file&%~dps0static-compress -c zopfli -j 8 -e .gz @file1>nul&%~dps0static-compress -c brotli -j 8 -e .br -q 0 @file1>nul)"
-forfiles.exe /s %lastdate% /m "*.json" /c "cmd /c if @isdir==FALSE (echo.@file&%~dps0static-compress -c zopfli -j 8 -e .gz @file1>nul&%~dps0static-compress -c brotli -j 8 -e .br -q 0 @file1>nul)"
-forfiles.exe /s %lastdate% /m "*.html" /c "cmd /c if @isdir==FALSE (echo.@file&%~dps0static-compress -c zopfli -j 8 -e .gz @file1>nul&%~dps0static-compress -c brotli -j 8 -e .br -q 0 @file1>nul)"
-forfiles.exe /s %lastdate% /m "*.xml" /c "cmd /c if @isdir==FALSE (echo.@file&%~dps0static-compress -c zopfli -j 8 -e .gz @file1>nul&%~dps0static-compress -c brotli -j 8 -e .br -q 0 @file1>nul)"
+forfiles.exe /s %lastdate% /m "*.css" /c "cmd.exe /c if @isdir==FALSE (\"%~0\" @file)"
+forfiles.exe /s %lastdate% /m "*.js" /c "cmd.exe /c if @isdir==FALSE (\"%~0\" @file)"
+forfiles.exe /s %lastdate% /m "*.json" /c "cmd.exe /c if @isdir==FALSE (\"%~0\" @file)"
+forfiles.exe /s %lastdate% /m "*.html" /c "cmd.exe /c if @isdir==FALSE (\"%~0\" @file)"
+forfiles.exe /s %lastdate% /m "*.xhtml" /c "cmd.exe /c if @isdir==FALSE (\"%~0\" @file)"
+forfiles.exe /s %lastdate% /m "*.xml" /c "cmd.exe /c if @isdir==FALSE (\"%~0\" @file)"
 ::static-compress.exe -c brotli -j 8 -e ".br" "**/*.{css,js,json,html]"
 cd robo_gzip
 echo.%date:~4%>__last_run__
